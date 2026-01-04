390fafca08c4c6610605c7d0b313df7d5ba2a3fa
this-is-eis <eisiel@disroot.org>
2026-01-04 14:08:27 +0700

    feat: :sparkles: add /advanced

--- scripts/register-commands.js
+++ scripts/register-commands.js
@@ -18,13 +18,98 @@ const GUILD_ID = process.env.DISCORD_GUILD_ID;
 const commands = [
 	{
 		name: "card",
-		description: "Search for a card by name",
+		description: "Search card(s) by name",
 		options: [
 			{
 				name: "name",
-				description: "The card name to search for",
+				description:
+					"The card name to search for (partial name allowed)",
 				type: 3, // STRING type
 				required: true,
+			},
+		],
+	},
+	{
+		name: "advanced",
+		description: "Search card(s) by advanced filters",
+		options: [
+			{
+				name: "name",
+				description:
+					"The card name to search for (partial name allowed)",
+				type: 3, // STRING type
+			},
+			{
+				name: "faction",
+				description: "Filter the results by faction",
+				type: 3, // STRING type
+				choices: [
+					{ name: "Nobility", value: "Nobility" },
+					{ name: "Uprising", value: "Uprising" },
+					{ name: "Clans", value: "Clans" },
+					{ name: "Gathering", value: "Gathering" },
+				],
+			},
+			{
+				name: "strength",
+				description: "Filter the results by strength",
+				type: 4, // INTEGER type
+			},
+			{
+				name: "archetype",
+				description: "Filter the results by archetype",
+				type: 3, // STRING type
+				choices: [
+					{ name: "Ruse", value: "Ruse" },
+					{ name: "Trader", value: "Trader" },
+					{ name: "Follower", value: "Follower" },
+					{ name: "Agent", value: "Agent" },
+					{ name: "Cavalry", value: "Cavalry" },
+					{ name: "War Machine", value: "War Machine" },
+					{ name: "Captain", value: "Captain" },
+					{ name: "Heir", value: "Heir" },
+					{ name: "Champion", value: "Champion" },
+				],
+			},
+			{
+				name: "trait",
+				description: "Filter the results by trait",
+				type: 3, // STRING type
+				choices: [
+					{ name: "Resilient", value: "Resilient" },
+					{ name: "Invulnerable", value: "Invulnerable" },
+					{ name: "Pathfinder", value: "Pathfinder" },
+				],
+			},
+			{
+				name: "command",
+				description: "Filter the results by command",
+				type: 3, // STRING type
+				choices: [
+					{ name: "Ambush", value: "Ambush" },
+					{ name: "Retreat", value: "Retreat" },
+					{ name: "Flank", value: "Flank" },
+					{ name: "Rally", value: "Rally" },
+					{ name: "Deploy", value: "Deploy" },
+					{ name: "Deadly", value: "Deadly" },
+				],
+			},
+			{
+				name: "action_step",
+				description: "Filter the results by action step",
+				type: 3, // STRING type
+				choices: [
+					{ name: "Spring", value: "Spring" },
+					{ name: "Summer", value: "Summer" },
+					{ name: "Autumn", value: "Autumn" },
+					{ name: "Winter", value: "Winter" },
+				],
+			},
+			{
+				name: "tags",
+				description:
+					"Filter the results by tags. ex: 'Wild Kingdom,Intrigue Suit'",
+				type: 3, // STRING type
 			},
 		],
 	},
